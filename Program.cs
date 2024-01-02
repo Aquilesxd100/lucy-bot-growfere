479e02d6fdbb81707372d8893324107bd979ebb2
fernando <fernandoalanfillmann@gmail.com>
2024-01-02 17:30:49 -0300

    feat: estrutura base e secrets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using bot_lucy_growfere.Secrets;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,34 @@ namespace bot_lucy_growfere
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
 
+        static async Task Main(string[] args)
+        {
+            SecretsReader secrets = new SecretsReader();
+            await secrets.GetSecrets();
+
+            DiscordConfiguration discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = secrets.botToken,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+        private static Task Client_Ready(
+            DiscordClient sender,
+            DSharpPlus.EventArgs.ReadyEventArgs args
+        ) {
+            return Task.CompletedTask;
         }
     }
 }
---
+++ Secrets/Secrets.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+namespace bot_lucy_growfere.Secrets
+{
+    public class SecretsReader {
+        public string botToken { get; set; }
+        public string dataBaseUrl { get; set; }
+
+        public async Task GetSecrets() {
+            using (StreamReader sr = new StreamReader("secretsData.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                SecretsData data = JsonConvert.DeserializeObject<SecretsData>(json);
+
+                this.botToken = data.botToken;
+                this.dataBaseUrl = data.dataBaseUrl;
+            }
+        }
+    }
+
+    internal sealed class SecretsData {
+        public string botToken { get; set; }
+        public string dataBaseUrl { get; set; }
+    }
+}
+
