208b7eb8012faf892fe8b8dc53e30d5c7afff04f
fernando <fernandoalanfillmann@gmail.com>
2024-01-02 09:42:02 -0300

    inicializacao repositorio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bot_lucy_growfere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
